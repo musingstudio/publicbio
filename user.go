a1d4f6bc88ad2f3c27de2c03f0f734b89757899e
Matt Baer <matt@write.as>
2018-05-30 21:42:42 -0400

    Support AvatarURL property on profile

--- user.go
+++ user.go
@@ -5,16 +5,15 @@ import (
 	"html/template"
 )
 
-type (
-	// Profile is a publicly-viewable user, containing only the data necessary
-	// to display a profile.
-	Profile struct {
-		Username string                   `json:"username"`
-		Name     converter.NullJSONString `json:"name"`
-		Header   converter.NullJSONString `json:"header"`
-		Bio      converter.NullJSONString `json:"bio"`
-	}
-)
+// Profile is a publicly-viewable user, containing only the data necessary
+// to display a profile.
+type Profile struct {
+	AvatarURL string                   `json:"avatar_url"`
+	Username  string                   `json:"username"`
+	Name      converter.NullJSONString `json:"name"`
+	Header    converter.NullJSONString `json:"header"`
+	Bio       converter.NullJSONString `json:"bio"`
+}
 
 func (p *Profile) RenderedBio() template.HTML {
 	return template.HTML(p.Bio.String)
